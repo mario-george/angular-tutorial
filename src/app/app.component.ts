f8cea306072925c0770307f7afc087478be64283
mario-george <vortexspades@gmail.com>
2023-12-31 19:04:06 +0200

    assignment solution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'angularMainProject';
+  title = 'angular-tutorial-basics';
+  showSecret = false;
+  logs = [];
+  onToggle() {
+    this.showSecret = !this.showSecret;
+    this.logs.push(this.logs.length + 1);
+  }
 }
